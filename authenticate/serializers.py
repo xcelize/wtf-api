7ad45fe4cb2907143049ec0bd04cb5e4015faba7
Alexis Petraz <apetraz@myriadev.fr>
2020-12-19 18:54:18 +0100

    HashPassword pour l'api

--- authenticate/serializers.py
+++ authenticate/serializers.py
@@ -22,5 +22,12 @@ class UserSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
 
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
 
 
