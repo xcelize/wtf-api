9951ce14a55a97c29460e85bcc0fc6017b8c2364
Alexis Petraz <apetraz@myriadev.fr>
2020-12-27 21:05:40 +0100

    change for JWT

--- WtfApi/settings.py
+++ WtfApi/settings.py
@@ -145,6 +145,11 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
