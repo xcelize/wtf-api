dc178f7c2e67b92f01de699c63a94625eabc58a5
Rémi Martinez <64494563+remi-martinez@users.noreply.github.com>
2021-01-13 17:22:57 +0100

    📄 Pagination 50 => 20

--- WtfApi/settings.py
+++ WtfApi/settings.py
@@ -155,7 +155,7 @@ Parametrage du module djangorest
 '''
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 20,
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
